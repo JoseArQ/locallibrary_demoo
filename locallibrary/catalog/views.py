8ba6ffd20280799b9d87a39ddc1df3f29ccdfe11
Jose Arroyave <arquos.14@gmail.com>
2021-11-07 15:50:03 -0500

    feature authors list and detail.

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -20,6 +20,7 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
 
     """def get_context_data(self, **kwargs):
         context = super(BookListView, self).get_context_data(**kwargs)
@@ -28,3 +29,9 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
